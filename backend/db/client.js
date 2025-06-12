d5695844b684c1027172909d0547430c9addbfbb
Bram Cohn <BramCohn@gmail.com>
2025-06-12 10:21:28 -0600

    Finished preliminary file structure

---
+++ backend/db/client.js
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432", 10),
+});
+// Test connection and log any errors during pool creation or connection
+pool.on("connect", () => {
+  console.log("Successfully connected to the PostgreSQL database pool");
+});
