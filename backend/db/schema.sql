5f7e18e1618063be7e7213fe18c6dfe3bf3a71e6
Matthew Sharp <sharpmatthew505@gmail.com>
2025-06-13 11:26:16 -0500

    added first and last name to schema.sql

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -26,6 +26,8 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL,
     unit INTEGER REFERENCES units(id) NOT NULL,
     is_manager BOOLEAN NOT NULL,
