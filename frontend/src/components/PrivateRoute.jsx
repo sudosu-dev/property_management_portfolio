1f0f8ef87a4c87977d785fe35f922315a4a8e7d1
Kai Wui Ye <kye5595@gmail.com>
2025-06-18 14:58:45 -0400

    preliminary form

--- frontend/src/components/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../auth/AuthContext";
-
-export default function PrivateRoute({ children }) {
-  const { token } = useAuth();
-  return token ? children : <Navigate to="/login" />;
-}
--- frontend/src/pages/Maintenance/Maintenance.jsx
+++ frontend/src/pages/Maintenance/Maintenance.jsx
@@ -1,35 +1,75 @@
 import { useState } from "react";
 import { useAuth } from "../../auth/AuthContext";
 import styles from "./Maintenance.module.css";
+import { API } from "../../api/ApiContext";
 
 function Maintenance() {
   const { user, token } = useAuth();
   const [formData, setFormData] = useState({});
-  const [status, setStatus] = useState("");
+  const [message, setMessage] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Submitting Request");
-    setFormData({ username: "", userPassword: "" });
+
+    if (!user || !token) {
+      alert("You must be logged in to submit a maintenance request.");
+      return;
+    }
+
+    try {
+      const response = await fetch(`${API}/maintenance`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          information: formData.information,
+        }),
+      });
+      if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.error || "Request faied.");
+      }
+      setMessage("Maintenance request submitted successfully.");
+      setFormData({ information: "" });
+    } catch (err) {
+      console.error("Submission error:", err.message);
+      setMessage("Failed to submit request");
+    }
   };
 
   return (
     <>
-      <h1>Maintenance</h1>
+      <h1>Maintenance Requests</h1>
       <div className={styles.requests}>
         <h2>Make New Maintenance Request</h2>
+        <div className={styles.info}>
+          <p>
+            <strong>Name:</strong> {`${user.first_name} ${user.last_name}`}
+          </p>
+          <p>
+            <strong>Unit: </strong> {`${user.unit}`}
+          </p>
+          <p>
+            <strong>Email: </strong> {`${user.email}`}
+          </p>
+        </div>
         <form onSubmit={handleSubmit}>
           <label>
-            Issue
+            <strong>Issue: </strong>
             <input
               value={formData.information}
               type="text"
-              name="information"
+              name="issue"
               onChange={(e) =>
                 setFormData({ ...formData, [e.target.name]: e.target.value })
               }
+              required
+              placeholder="Please describe the issue..."
             />
           </label>
+          <button type="submit">Submit Request</button>
         </form>
       </div>
     </>
