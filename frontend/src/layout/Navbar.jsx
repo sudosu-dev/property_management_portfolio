6abb3d3de854bbbabb8fcc58987fba9b9721fb65
aidanhenning <henningaidan@gmail.com>
2025-06-17 22:06:32 -0700

    added starter routes for navbar and basic styling. Also set up index css to remove default padding and margin. Set up global variable colors for use throughout site

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,3 +1,5 @@
+import styles from "./Navbar.module.css";
+
 import { NavLink } from "react-router";
 
 import { useAuth } from "../auth/AuthContext";
@@ -5,15 +7,24 @@ import { useAuth } from "../auth/AuthContext";
 export default function Navbar() {
   const { token, logout } = useAuth();
   return (
-    <header id="navbar">
-      <NavLink id="brand" to="/">
-        <p>Frontend Template</p>
+    <header className={styles.navbar}>
+      <NavLink className={styles.brand} to="/">
+        <p>Logo Placeholder</p>
       </NavLink>
       <nav>
         {token ? (
-          <button onClick={logout}>Log out</button>
+          <div className={styles.loggedInNav}>
+            <NavLink to="/dashboard">Dashboard</NavLink>
+            <NavLink to="/profile">profile</NavLink>
+            <NavLink to="/payments">payments</NavLink>
+            <NavLink to="/maintenance">maintenance</NavLink>
+            <button onClick={logout}>Log out</button>
+          </div>
         ) : (
-          <NavLink to="/login">Log in</NavLink>
+          <div className={styles.landingPageNav}>
+            <NavLink to="/login">Log in</NavLink>
+            <NavLink to="/about">about</NavLink>
+          </div>
         )}
       </nav>
     </header>
