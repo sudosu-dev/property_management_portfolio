63fc952f0d41569e9f07080ccf45d80c3f364cdd
aidanhenning <henningaidan@gmail.com>
2025-06-20 11:11:21 -0700

    added announcements to navbar

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,7 +1,5 @@
 import styles from "./Navbar.module.css";
-
 import { NavLink } from "react-router";
-
 import { useAuth } from "../auth/AuthContext";
 
 export default function Navbar() {
@@ -12,18 +10,39 @@ export default function Navbar() {
         <p>Logo Placeholder</p>
       </NavLink>
       <nav>
+        {}
         {token ? (
           <div className={styles.loggedInNav}>
-            <NavLink to="/dashboard">Dashboard</NavLink>
-            <NavLink to="/profile">Profile</NavLink>
-            <NavLink to="/payments">Payments</NavLink>
-            <NavLink to="/maintenance">Maintenance</NavLink>
-            <button onClick={logout}>Logout</button>
+            <NavLink to="/dashboard" className={styles.links}>
+              Dashboard
+            </NavLink>
+            <NavLink to="/payments" className={styles.links}>
+              Payments
+            </NavLink>
+            <NavLink to="/maintenance" className={styles.links}>
+              Maintenance
+            </NavLink>
+            <NavLink to="/announcements" className={styles.links}>
+              Announcements
+            </NavLink>
+            <NavLink to="/profile" className={styles.links}>
+              Profile
+            </NavLink>
+            <button onClick={logout} className={styles.logoutButton}>
+              Logout
+            </button>
           </div>
         ) : (
           <div className={styles.landingPageNav}>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/about">About</NavLink>
+            <NavLink to="/about" className={styles.links}>
+              About
+            </NavLink>
+            <NavLink to="/login" className={styles.links}>
+              Property Managers
+            </NavLink>
+            <NavLink to="/login" className={styles.loginButton}>
+              Resident Login
+            </NavLink>
           </div>
         )}
       </nav>
