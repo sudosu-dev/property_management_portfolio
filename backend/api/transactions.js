b52a4ac99958d73af34ce6e35640e03afaea158f
Matthew Sharp <sharpmatthew505@gmail.com>
2025-06-21 15:43:28 -0500

    added getAllPayments fucntion and endpoint to back end

--- backend/api/transactions.js
+++ backend/api/transactions.js
@@ -1,6 +1,9 @@
 import express from "express";
 import requireUser from "#middleware/requireUser";
-import { getTransactionsByUserId } from "#db/queries/transactions";
+import {
+  getTransactionsByUserId,
+  getAggregatedBalances,
+} from "#db/queries/transactions";
 
 const router = express.Router();
 
@@ -12,6 +15,15 @@ router.get("/my-history", async (req, res) => {
     res.json(history);
   } catch (error) {
     res.status(500).json({ error: "Failed to retrieve transaction history." });
+  }
+});
+
+router.get("/all-balances", requireManager, async (req, res) => {
+  try {
+    const balance = await getAggregatedBalances();
+    res.json(balance);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve tenant balances." });
   }
 });
 
